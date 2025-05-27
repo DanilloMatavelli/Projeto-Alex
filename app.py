1871d5fe723f3891ad692f9d8639a954eaf1d8e0
Danillo Matavelli <danillomatavelli26@gmail.com>
2025-05-27 09:30:07 -0300

    Criando a tela de cadastro e colocando section. e criando a rota com a função.

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def principal():
     return render_template('pagina_principal.html')
 
+@app.route('/login')
+def pagina_login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

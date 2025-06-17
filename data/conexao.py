571809cfa7ee105d45dccf3883fff714cbe1d067
Matheus Alves <matheus.jeje2@gmail.com>
2025-06-17 09:29:31 -0300

    conexao

--- data/conexao.py
+++ data/conexao.py
@@ -4,7 +4,8 @@ def conectar():
     conexao = mysql.connector.connect(
         host="db-gptrio-gptrio.c.aivencloud.com",  
         user="avnadmin",            
-        password="AVNS_k7-exgoO4kambzp3PPU",    
+        password="AVNS_k7-exgoO4kambzp3PPU",
+        port = "17333",    
         database="db_gptrio"     
     )
     return conexao

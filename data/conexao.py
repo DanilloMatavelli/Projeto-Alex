a44400e4a4cf237a23c4ee7f5e97a70d5efdbc77
Danillo Matavelli <danillomatavelli26@gmail.com>
2025-06-17 08:54:15 -0300

    ADD: conexao com banco de dados online

--- data/conexao.py
+++ data/conexao.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def conectar():
     conexao = mysql.connector.connect(
-        host="localhost",  
-        user="root",            
-        password="root",    
-        database="db_gptrio"     
+        host="db-gptrio-gptrio.c.aivencloud.com",  
+        user="avnadmin",            
+        password="AVNS_k7-exgoO4kambzp3PPU",    
+        database="db_gptrio"    
     )
     return conexao

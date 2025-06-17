07684ff9243a72b9c7e38c0068977df3a933e594
Matheus Alves <matheus.jeje2@gmail.com>
2025-06-17 09:47:23 -0300

    Conexão banco de dados aivem

--- data/conexao.py
+++ data/conexao.py
@@ -2,9 +2,10 @@ import mysql.connector
 
 def conectar():
     conexao = mysql.connector.connect(
-        host="localhost",  
-        user="root",            
-        password="root",    
+        host="db-gptrio-gptrio.c.aivencloud.com",  
+        user="avnadmin", 
+        port = "17333",           
+        password="AVNS_k7-exgoO4kambzp3PPU",    
         database="db_gptrio"     
     )
     return conexao
